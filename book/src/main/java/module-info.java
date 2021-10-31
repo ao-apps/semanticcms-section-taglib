3abc087c747b77958c46d1e3ce9c06006ddc1b24
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:45:23 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- book/src/main/java/module-info.java
+++ book/src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with semanticcms-section-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ * along with semanticcms-section-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.semanticcms.section.taglib.book {
 	exports com.semanticcms.section.taglib.book;
