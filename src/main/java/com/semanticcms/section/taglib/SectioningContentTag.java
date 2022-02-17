d54d009a66e2855b8a0be3e63e56be17f8c17b29
AO Industries, Inc <support@aoindustries.com>
2022-02-16 19:23:06 -0600

    Resolved unused private fields

--- src/main/java/com/semanticcms/section/taglib/SectioningContentTag.java
+++ src/main/java/com/semanticcms/section/taglib/SectioningContentTag.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-section-taglib - Sections nested within SemanticCMS pages and elements in a JSP environment.
- * Copyright (C) 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -72,7 +72,6 @@ public abstract class SectioningContentTag<SC extends SectioningContent> extends
 	private PageIndex pageIndex;
 	private Serialization serialization;
 	private Doctype doctype;
-	private boolean indent;
 
 	@Override
 	protected void doBody(SC sectioningContent, CaptureLevel captureLevel) throws JspException, IOException {
