7e9e578cb5da543f3cd7c83bed7f9fdd10593bca
AO Industries, Inc <support@aoindustries.com>
2022-04-27 00:14:49 -0500

    Performed automatic clean-up with org.openrewrite.java.cleanup.Cleanup

--- src/main/java/com/semanticcms/section/taglib/SectioningContentTag.java
+++ src/main/java/com/semanticcms/section/taglib/SectioningContentTag.java
@@ -100,7 +100,7 @@ public abstract class SectioningContentTag<SC extends SectioningContent> extends
     writeTo(
         request,
         new Document(serialization, doctype, characterEncoding, out)
-            .setAutonli(false) // Do not add extra newlines to JSP
+            .setAutonli(false)// Do not add extra newlines to JSP
             .setIndent(false), // Do not add extra indentation to JSP
         context,
         pageIndex
